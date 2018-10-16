1c26b5c362ba172f37e69e0f16482e0513f3d7e5
Juliani Schlickmann Damasceno <nany.julychi@gmail.com>
2018-10-15 20:20:37 -0700

    Dicee app

---
+++ Dicee/DiceePlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myAge : Int = 22;
+
+myAge = 23;
+
+let myName : String = "Juliani"
+
+let myFullName = "\(myName) Schlickmann Dmasceno"
+
+let myDetails = "\(myName), \(myAge)"
